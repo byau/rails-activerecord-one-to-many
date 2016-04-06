0fef19248bccd0a351fa4de375fc591309ff4d90
byau <billy.l.yau@gmail.com>
2016-04-06 13:03:45 -0400

    code along

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,3 @@
 class City < ActiveRecord::Base
+  has_many :people, inverse_of: :born_in, foreign_key: 'born_in_id'
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,10 @@
+# class Person < ActiveRecord::Base
+#   def full_name
+#     "#{surname}, #{given_name}"
+#   end
+# end
+
 class Person < ActiveRecord::Base
-  def full_name
-    "#{surname}, #{given_name}"
-  end
+  has_many :pets, inverse_of: :owner, foreign_key: 'owner_id'
+  belongs_to :born_in, inverse_of: :people, class_name: 'City'
 end
